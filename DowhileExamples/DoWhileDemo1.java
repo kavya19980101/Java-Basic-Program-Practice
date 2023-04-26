f1fb41c26c372e28b2336227c1e0ffd8909063d5
kavya19980101 <kavyastestings@gmail.com>
2023-04-26 19:43:35 +0530

    Initial commit

---
+++ DowhileExamples/DoWhileDemo1.java
@@ -0,0 +1,12 @@
+package DowhileExamples;
+
+public class DoWhileDemo1 {
+    public static void main(String args[])
+    {
+        int i =1;
+                do{
+                    System.out.println(i);
+                i++;}
+                while(i<=10);
+    }
+}
