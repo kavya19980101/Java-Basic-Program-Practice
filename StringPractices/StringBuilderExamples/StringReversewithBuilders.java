fcee8a418b702fddb7d6dd812717c2f6383f98d3
kavya19980101 <kavyastestings@gmail.com>
2023-05-08 10:13:26 +0530

    Changes

---
+++ StringPractices/StringBuilderExamples/StringReversewithBuilders.java
@@ -0,0 +1,14 @@
+package StringPractices.StringBuilderExamples;
+
+import java.util.Scanner;
+
+public class StringReversewithBuilders {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        StringBuilder str1;
+        System.out.println("Enter a String");
+        str1= new StringBuilder(sc.nextLine());
+        str1.reverse();
+                System.out.println("The Reverse String is "+str1);
+    }
+}
