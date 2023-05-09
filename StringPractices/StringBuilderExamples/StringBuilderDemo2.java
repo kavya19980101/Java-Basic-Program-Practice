d879711ccbd4fa5b8acba213f2fa3e0162b8b29a
kavya19980101 <kavyastestings@gmail.com>
2023-05-09 16:52:43 +0530

    StringBuilder Practices

--- StringPractices/StringBuilderExamples/StringBuilderDemo2.java
+++ StringPractices/StringBuilderExamples/StringBuilderDemo2.java
@@ -4,7 +4,7 @@ public class StringBuilderDemo2 {
     public static void main(String args[])
     {
         StringBuilder str1=new StringBuilder("Kavya");
-        System.out.println(str1.append("Shekar"));//Appends the specified character or object to the end of the string.
+        System.out.println(str1.append("Sheka"));//Appends the specified character or object to the end of the string.
         String Designation=" Test Engineer";
         System.out.println(str1.insert(11,Designation));//Inserts the specified character or object at the specified index in the string
         System.out.println(str1.delete(2,4));//Deletes the characters from the string between the specified start and end indices.
@@ -16,8 +16,8 @@ public class StringBuilderDemo2 {
         System.out.println(str1.length());//Returns the length of the string.
         str1.setCharAt(1, 'x');//Sets the character at the specified index in the string to the specified character.
         System.out.println(str1);
-        System.out.println(str1.capacity());//Returns the current capacity of the string builder.
-
+        System.out.println(str1.capacity());
+////////
 
 
 
