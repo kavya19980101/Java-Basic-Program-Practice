f2facfd5af1006b7cfdfd00ff1764158b7641df8
kavya19980101 <kavyastestings@gmail.com>
2023-05-13 18:11:23 +0530

    Encapsulation concepts Demo1 for understanding purpose practiced

--- Encapsulation/Book.java
+++ Encapsulation/Book.java
@@ -1,5 +1,27 @@
 package Encapsulation;
 
 public class Book {
+   private String TitleofBook;
+   private int NoofCopiesSold;
 
-}
+   public String getTitle()
+   {
+       return TitleofBook;
+   }
+   public void setTitle(String TitleofBook)
+   {
+this.TitleofBook=TitleofBook;
+   }
+
+   public int getNoofCopiesSold(){
+       return NoofCopiesSold;
+   }
+   public void SetNocopiessold(int NoofCopiesSold) {
+
+       if (NoofCopiesSold > 0) {
+           this.NoofCopiesSold = NoofCopiesSold;
+       } else
+           System.out.println("You cant set Invalid values");
+
+   }
+   }
---
+++ Encapsulation/BookRunner.java
@@ -0,0 +1,15 @@
+package Encapsulation;
+
+public class BookRunner {
+    public static void main(String[] args) {
+Book B1=new Book();
+B1.setTitle("Java Programming");
+System.out.println(B1.getTitle());
+B1.SetNocopiessold(100);
+System.out.println(B1.getNoofCopiesSold());
+
+
+
+
+    }
+}
