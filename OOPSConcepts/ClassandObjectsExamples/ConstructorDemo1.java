e1930c85f8e1367c0787fe1fe916d7bb8ba6fef0
kavya19980101 <kavyastestings@gmail.com>
2023-05-13 14:40:16 +0530

    Constuctor Demo examples practices

---
+++ OOPSConcepts/ClassandObjectsExamples/ConstructorDemo1.java
@@ -0,0 +1,14 @@
+package OOPSConcepts.ClassandObjectsExamples;
+
+public class ConstructorDemo1 {
+
+
+    ConstructorDemo1(String name,int age)
+    {
+        System.out.println("My name is" +name);
+        System.out.println("My age is"+age);
+    }
+
+
+
+}
---
+++ OOPSConcepts/ClassandObjectsExamples/ConstructorDemo1Runner.java
@@ -0,0 +1,16 @@
+package OOPSConcepts.ClassandObjectsExamples;
+
+import java.util.concurrent.Callable;
+
+public class ConstructorDemo1Runner {
+    public static void main(String[] args) {
+System.out.println("Enter the details of Student1");
+ConstructorDemo1 S1= new ConstructorDemo1(" Kavya",25);
+System.out.println("Enter the details of Student1");
+ConstructorDemo1 S3= new ConstructorDemo1(" Apoorva",25);
+
+
+
+
+    }
+}
